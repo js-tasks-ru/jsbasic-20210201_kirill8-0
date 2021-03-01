93a130c7513b9ed7d59faa33afa8e012d74653fe
kirill <kirill8.0@mail.ru>
2021-03-01 23:56:23 +0300

    modul 2 done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,22 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+function sumSalary(salaries) {
+  
+    let sum = 0;
+  
+    for (let sal in salaries) {
+      
+      if (Number.isFinite(salaries[sal])) {
+       sum += salaries[sal];
+      } else{sum += 0}
+       }
+      return(sum)
+      }
+  sumSalary(salaries)
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,18 @@
 function isEmpty(obj) {
   // ваш код...
-}
+  for (let check in obj){
+   
+  return false;
+  }
+  return true;
+  
+ }
+ 
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) ); // false
