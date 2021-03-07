240a1ceb1b9da2d083e384dca103e4e32b510f6f
kirill <kirill8.0@mail.ru>
2021-03-07 17:24:17 +0300

    3module complited

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,19 @@
+
+
 function getMinMax(str) {
-  // ваш код...
+
+  // ...
+  let arr = str.split(',')
+  arr = arr.join(' ')
+  arr = arr.split(' ')
+  let numbers = arr.map(number => +number).filter(number => !isNaN(number))  
+/*   return arr
+   */ 
+   
+  return {min : Math.min(...numbers),
+    max : Math.max(...numbers),
 }
+
+  }
+
+getMinMax(inputData); // { min: -5.8, max: 73  }
